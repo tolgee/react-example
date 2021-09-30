81a68c44659df804f4b6c36d6b1991deb4e1477b
Jan Cizmar <cizmar@chlupac.com>
2021-09-30 13:36:25 +0200

    feat(js sdk): Enable default value providing TG-335

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,55 @@
-import React from 'react';
-import './App.css';
-import {TolgeeProvider} from "@tolgee/react";
-import {UI} from "@tolgee/ui"
-import {Page} from "./Page";
-
+import React from "react";
+import "./App.css";
+import { TolgeeProvider } from "@tolgee/react";
+import { UI } from "@tolgee/ui";
+import { Page } from "./Page";
 
 const App = () => {
-    return (
-        <TolgeeProvider
-            filesUrlPrefix="i18n/"
-            apiUrl={process.env.REACT_APP_TOLGEE_API_URL}
-            apiKey={process.env.REACT_APP_TOLGEE_API_KEY}
-            ui={process.env.REACT_APP_TOLGEE_WITH_UI === "true" ? UI : undefined}
-            availableLanguages={["en", "cs"]}
-            loadingFallback={
-                <div style={{
-                    display: "flex",
-                    height: "100vh",
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                    Loading...
-                </div>
-            }
+  return (
+    <TolgeeProvider
+      filesUrlPrefix="i18n/"
+      apiUrl={process.env.REACT_APP_TOLGEE_API_URL}
+      apiKey={process.env.REACT_APP_TOLGEE_API_KEY}
+      ui={process.env.REACT_APP_TOLGEE_API_KEY ? UI : undefined}
+      availableLanguages={["en", "cs"]}
+      preloadFallback={true}
+      loadingFallback={
+        <div
+          style={{
+            display: "flex",
+            height: "100vh",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-            <div style={{
-                width: "800px",
-                margin: "0 auto 0 auto",
-                textAlign: "center",
-                paddingTop: "20px"
-            }}>
-                <div style={{padding: "20px"}}>
-                    <img src="favicon.svg" style={{
-                        width: "100px",
-                        height: "100px"
-                    }}/>
-                </div>
+          Loading...
+        </div>
+      }
+    >
+      <div
+        style={{
+          width: "800px",
+          margin: "0 auto 0 auto",
+          textAlign: "center",
+          paddingTop: "20px",
+        }}
+      >
+        <div style={{ padding: "20px" }}>
+          <img
+            src="favicon.svg"
+            alt="Logo"
+            style={{
+              width: "100px",
+              height: "100px",
+            }}
+          />
+        </div>
 
-                <h1 style={{marginBottom: "50px"}}>React with Tolgee Example App</h1>
-                <Page/>
-            </div>
-        </TolgeeProvider>
-    )
+        <h1 style={{ marginBottom: "50px" }}>React with Tolgee Example App</h1>
+        <Page />
+      </div>
+    </TolgeeProvider>
+  );
 };
 
 export default App;
--- src/Page.tsx
+++ src/Page.tsx
@@ -1,23 +1,47 @@
 import * as React from "react";
-import {FunctionComponent} from "react";
-import {T, useTranslate} from "@tolgee/react";
-import {ChooseLanguage} from "./ChooseLanguage";
+import { FunctionComponent } from "react";
+import { T, useTranslate } from "@tolgee/react";
+import { ChooseLanguage } from "./ChooseLanguage";
 
 export const Page: FunctionComponent<{}> = () => {
-    const t = useTranslate();
+  const t = useTranslate();
 
-    return (
-        <div>
-            <ChooseLanguage/>
+  return (
+    <div>
+      <ChooseLanguage />
 
-            <h1><T>hello_world</T></h1>
-            <p><T>Texts in attributes can also be translated with tolgee\.</T></p>
-            <p><T parameters={{dogsCount: "5"}}>Peter has n dogs</T></p>
-            <input placeholder={t("i_am_translated_placeholder")}/>
-            <p><T>And also! You can translate text inside options of select!</T></p>
-            <select>
-                <option>{t("hi_i_am_translated_option")}</option>
-            </select>
-        </div>
-    )
-}
+      <h1>
+        <T>hello_world</T>
+      </h1>
+      <p>
+        <T>Texts in attributes can also be translated with tolgee\.</T>
+      </p>
+      <p>
+        <T parameters={{ dogsCount: "5" }}>Peter has n dogs</T>
+      </p>
+      <input placeholder={t("i_am_translated_placeholder")} />
+      <p>
+        <T>And also! You can translate text inside options of select!</T>
+      </p>
+      <select>
+        <option>{t("hi_i_am_translated_option")}</option>
+      </select>
+      <h1>
+        <T>providing_default_values</T>
+      </h1>
+
+      <p>
+        <T keyName="key_with_default">This is default value!</T>
+      </p>
+      <p>
+        {t({
+          key: "key_to_translate",
+          parameters: { param: "value" },
+          noWrap: false,
+          defaultValue: "This is default",
+        })}
+      </p>
+      <p>{t("key_to_translate", {}, true, "This is also default!")}</p>
+    </div>
+  );
+};
