22166fcc5703978b6d6f80d08781ccda94c8d776
Tolgee Automation Machine <machine@tolgee.io>
2021-11-16 09:32:25 +0000

    chore: Publishing example from release:

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,33 @@
-import React from "react";
-import "./App.css";
-import { TolgeeProvider } from "@tolgee/react";
-import { UI } from "@tolgee/ui";
-import { Page } from "./Page";
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { TolgeeProvider } from '@tolgee/react';
+import { Todos } from './Todos';
+import { TranslationMethods } from './TranslationMethods';
 
-const App = () => {
+export const App = () => {
+  const currentRoute = window.location.pathname;
+
   return (
     <TolgeeProvider
-      filesUrlPrefix="i18n/"
+      staticData={{
+        en: () => import('./i18n/en.json'),
+        de: () => import('./i18n/de.json'),
+        cs: () => import('./i18n/cs.json'),
+        fr: () => import('./i18n/fr.json'),
+      }}
       apiUrl={process.env.REACT_APP_TOLGEE_API_URL}
       apiKey={process.env.REACT_APP_TOLGEE_API_KEY}
-      ui={process.env.REACT_APP_TOLGEE_API_KEY ? UI : undefined}
-      availableLanguages={["en", "cs"]}
-      preloadFallback={true}
-      loadingFallback={
-        <div
-          style={{
-            display: "flex",
-            height: "100vh",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          Loading...
-        </div>
+      loadingFallback={<div>Loading...</div>}
+      ui={
+        process.env.REACT_APP_TOLGEE_API_KEY
+          ? require('@tolgee/ui').UI
+          : undefined
       }
     >
-      <div
-        style={{
-          width: "800px",
-          margin: "0 auto 0 auto",
-          textAlign: "center",
-          paddingTop: "20px",
-        }}
-      >
-        <div style={{ padding: "20px" }}>
-          <img
-            src="favicon.svg"
-            alt="Logo"
-            style={{
-              width: "100px",
-              height: "100px",
-            }}
-          />
-        </div>
-
-        <h1 style={{ marginBottom: "50px" }}>React with Tolgee Example App</h1>
-        <Page />
-      </div>
+      {currentRoute === '/translation-methods' ? (
+        <TranslationMethods />
+      ) : (
+        <Todos />
+      )}
     </TolgeeProvider>
   );
 };
-
-export default App;
