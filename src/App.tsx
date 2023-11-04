3ffbe19621e9d579037eeb8575513536a2fb549c
Štěpán Granát <granat.stepan@gmail.com>
2023-11-04 15:47:39 +0100

    feat: shadow dom example

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import { Tolgee, TolgeeProvider, BackendFetch, DevTools } from '@tolgee/react';
+import { TolgeeProvider, TolgeeInstance } from "@tolgee/react";
 
-import { Todos } from './Todos';
-import { TranslationMethods } from './TranslationMethods';
-import { FormatIcu } from '@tolgee/format-icu';
+import { Todos } from "./Todos";
+import { TranslationMethods } from "./TranslationMethods";
 
-const tolgee = Tolgee()
-  .use(DevTools())
-  .use(FormatIcu())
-  .use(BackendFetch())
-  .init({
-    availableLanguages: ['en', 'cs', 'fr', 'de'],
-    apiUrl: process.env.REACT_APP_TOLGEE_API_URL,
-    apiKey: process.env.REACT_APP_TOLGEE_API_KEY,
-    projectId: process.env.REACT_APP_TOLGEE_PROJECT_ID,
-    fallbackLanguage: 'en',
-    defaultLanguage: 'en',
-  });
-
-export const App = () => {
+export const App = ({ tolgee }: { tolgee: TolgeeInstance }) => {
   const currentRoute = window.location.pathname;
 
   return (
     <TolgeeProvider tolgee={tolgee}>
-      {currentRoute === '/translation-methods' ? (
+      {currentRoute === "/translation-methods" ? (
         <TranslationMethods />
       ) : (
         <Todos />
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import { App } from './App';
 import { createRoot } from 'react-dom/client';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { Tolgee, BackendFetch, DevTools } from '@tolgee/react';
+import { FormatIcu } from '@tolgee/format-icu';
 
-const container = document.getElementById('root')!;
+const createShadowRoot = (root: Element): HTMLDivElement => {
+  const shadowRoot = root.attachShadow({ mode: 'open' });
+
+  const style = document.createElement('style');
+  style.textContent = `
+    #app-shadow {
+      all: initial;
+    }
+  `;
+  shadowRoot.appendChild(style);
+  const appContainer = document.createElement('div');
+  appContainer.id = `app-shadow`;
+  shadowRoot.appendChild(appContainer);
+  return appContainer;
+}
+
+const container = createShadowRoot(document.getElementById('root')!);
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+const tolgee = Tolgee()
+  .use(DevTools())
+  .use(FormatIcu())
+  .use(BackendFetch())
+  .init({
+    availableLanguages: ['en', 'cs', 'fr', 'de'],
+    apiUrl: process.env.REACT_APP_TOLGEE_API_URL,
+    apiKey: process.env.REACT_APP_TOLGEE_API_KEY,
+    projectId: process.env.REACT_APP_TOLGEE_PROJECT_ID,
+    fallbackLanguage: 'en',
+    defaultLanguage: 'en',
+    observerOptions: {
+      targetElement: container
+    }
+  });
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App tolgee={tolgee} />
   </React.StrictMode>
 );
