9b73079b2e9c57b74d4d25b83a532ac1f0f6aacf
Štěpán Granát <granat.stepan@gmail.com>
2023-11-07 14:37:19 +0100

    feat: add tolgee back in

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { Todos } from "./Todos";
 
 import { Tolgee, DevTools, FormatSimple, TolgeeProvider } from "@tolgee/react";
 
-const tolgee = Tolgee().use(DevTools()).use(FormatSimple()).init({
-  language: "en",
+const tolgee = Tolgee()
+  .use(DevTools())
+  .use(FormatSimple())
+  .init({
+    language: "en",
 
-  // for development
-  apiUrl: process.env.REACT_APP_TOLGEE_API_URL,
-  apiKey: process.env.REACT_APP_TOLGEE_API_KEY,
-});
+    // for development
+    apiUrl: process.env.REACT_APP_TOLGEE_API_URL,
+    apiKey: process.env.REACT_APP_TOLGEE_API_KEY,
+    staticData: {
+      en: () => import("./i18n/en.json"),
+      cs: () => import("./i18n/cs.json"),
+      de: () => import("./i18n/de.json"),
+    },
+  });
 
 export const App = () => {
   return (
